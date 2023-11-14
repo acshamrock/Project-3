1a73cda19e094217c79e9ae5e3d2ce5638db133c
acshamrock <brendan.dwyer17@gmail.com>
2023-11-14 10:56:32 -0600

    added twitter widget

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
+document.write('<a class="twitter-timeline" href="https://twitter.com/ILTechAthletics?ref_src=twsrc%5Etfw">Tweets by ILTechAthletics</a>');
+document.write('<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>');
 
