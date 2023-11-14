efe50b26e013b42fdfcab5d84f7a98ad0f3816e7
acshamrock <brendan.dwyer17@gmail.com>
2023-11-14 12:56:24 -0600

    added date and time

--- js/datetime.js
+++ js/datetime.js
@@ -1,1 +1,23 @@
+function showDateTime() {
+  var currentDate = new Date();
+
+  var date = currentDate.toDateString();
+  var time = currentDate.toLocaleTimeString();
+
+  console.log('Current Date: ' + date);
+  console.log('Current Time: ' + time);
+
+  var dateElement = document.createElement('p');
+  dateElement.textContent = 'Current Date: ' + date;
+
+  var timeElement = document.createElement('p');
+  timeElement.textContent = 'Current Time: ' + time;
+
+  document.body.appendChild(dateElement);
+  document.body.appendChild(timeElement);
+}
+
+window.onload = function() {
+  showDateTime();
+};
 
